6641ea905ad69ac1253b7903474993846b7351a7
Denis <kras_den@rambler.ru>
2016-04-03 23:31:55 +0700

    переделка проекта под с++

--- cart/src/model/MainContext.cpp
+++ cart/src/model/MainContext.cpp
@@ -1,11 +1,52 @@
 #include "MainContext.h"
 
-MainContext::MainContext()
+MainContext::MainContext(cl_device_type dev_type)
 {
     data = new MainContextData;
+
+    if (clGetPlatformIDs(0, NULL, &data->plat_count) == CL_SUCCESS)
+    {
+        data->plat = new cl_platform_id_t[data->plat_count];
+
+        if (clGetPlatformIDs(data->plat_count, data->plat, NULL) == CL_SUCCESS)
+        {
+            data->dev_count = new cl_uint_t[data->plat_count];
+            data->dev = new cl_device_id_t*[data->plat_count];
+            data->context = new cl_context_t*[data->plat_count];
+            data->cmd = new cl_command_queue_t*[data->plat_count];
+
+            for (cl_uint_t p = 0; p < data->plat_count; p++)
+            {
+                if (clGetDeviceIDs(data->plat[p],
+                                   dev_type,
+                                   0,
+                                   NULL,
+                                   &data->dev_count[p]) == CL_SUCCESS)
+                {
+
+                }
+                else
+                {
+
+                }
+            }
+        }
+        else
+        {
+
+        }
+    }
+    else
+    {
+    }
 }
 
 MainContext::~MainContext()
 {
+    delete[] data->cmd;
+    delete[] data->context;
+    delete[] data->dev;
+    delete[] data->dev_count;
+    delete[] data->plat;
     delete data;
 }
--- cart/src/model/MainContext.h
+++ cart/src/model/MainContext.h
@@ -1,36 +1,39 @@
 #ifndef __MAIN_CONTEXT_H
 #define __MAIN_CONTEXT_H
 
-#include <map>
-using namespace std;
-
 #define CL_USE_DEPRECATED_OPENCL_2_0_APIS
 
 #include <CL/cl.h>
 
-typedef int PlatIndex;
-typedef int DevIndex;
+typedef int plat_index_t;
+typedef int dev_index_t;
+
+typedef cl_uint cl_uint_t;
+typedef cl_device_id cl_device_id_t;
+typedef cl_platform_id cl_platform_id_t;
+typedef cl_context cl_context_t;
+typedef cl_command_queue cl_command_queue_t;
 
 struct MainContextData
 {
-    cl_uint total_dev_count = 0;
-    map<PlatIndex, cl_uint> dev_on_plat;
-    map<PlatIndex, map<DevIndex, cl_device_id>> devices;
+    cl_uint_t total_dev_count = 0;
+    cl_uint_t *dev_count;
+    cl_device_id_t **dev;
 
-    cl_uint platforms_count = 0;
-    map<PlatIndex, cl_platform_id> platforms;
+    cl_uint_t plat_count = 0;
+    cl_platform_id_t *plat;
 
-    map<PlatIndex, map<DevIndex, cl_context>> contexts;
-    map<PlatIndex, map<DevIndex, cl_command_queue>> cmd_queues;
+    cl_context_t **context;
+    cl_command_queue_t **cmd;
 };
 
 class MainContext
 {
-public:
-    MainContext();
-    ~MainContext();
 private:
     struct MainContextData *data;
+public:
+    MainContext(cl_device_type dev_type);
+    ~MainContext();
 };
 
 #endif // __MAIN_CONTEXT_H

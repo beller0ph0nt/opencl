88c1325db5f2814728d7038fa405425748dd6c3a
Denis <kras_den@rambler.ru>
2016-04-30 00:28:46 +0700

    работа над потоками

--- avg/kernel_avg.c
+++ avg/kernel_avg.c
@@ -1,46 +1,12 @@
+#include <pthread.h>
+
 #include "kernel_avg.h"
 
 void kernel_avg_calc(const context_t* context,
                      const kernel_t* kernel,
-                     avg_params_t *params)
+                     struct avg_params_t *params)
 {
-    struct par_info_t
-    {
-        double* start;
-        unsigned long int len;
-        cl_mem mem;
-    };
-
-    struct work_size_t
-    {
-        size_t global;
-        size_t local;
-    };
-
-    struct work_info_t
-    {
-        struct work_size_t size;
-    };
-
-    struct time_info_t
-    {
-        cl_ulong queued;
-        cl_ulong submit;
-        cl_ulong start;
-        cl_ulong end;
-    };
-
-    struct block_t
-    {
-        struct par_info_t left;
-        struct par_info_t right;
-        struct par_info_t out;
-        struct work_info_t work;
-        cl_event event;
-        struct time_info_t time;
-    };
-
-    struct block_t** blocks = NULL;
+    struct avg_block_t** blocks = NULL;
 
     double* cur_pointer = (double*) params->in;
 
@@ -89,4 +55,9 @@ void kernel_avg_calc(const context_t* context,
 
         // вызов нового потока
     }
+}
+
+void* thread_func(void* arg)
+{
+    struct avg_block_t* block = (struct avg_block_t*) arg;
 }
--- avg/kernel_avg.h
+++ avg/kernel_avg.h
@@ -4,13 +4,49 @@
 #include "kernel.h"
 #include "context.h"
 
-typedef struct {
+struct avg_par_info_t
+{
+    double* start;
+    unsigned long int len;
+    cl_mem mem;
+};
+
+struct avg_work_size_t
+{
+    size_t global;
+    size_t local;
+};
+
+struct avg_work_info_t
+{
+    struct avg_work_size_t size;
+};
+
+struct avg_time_info_t
+{
+    cl_ulong queued;
+    cl_ulong submit;
+    cl_ulong start;
+    cl_ulong end;
+};
+
+struct avg_block_t
+{
+    struct avg_par_info_t left;
+    struct avg_par_info_t right;
+    struct avg_par_info_t out;
+    struct avg_work_info_t work;
+    cl_event event;
+    struct avg_time_info_t time;
+};
+
+struct avg_params_t {
     double* in;
     unsigned long int len;
-} avg_params_t;
+};
 
 void kernel_avg_calc(const context_t* context,
                      const kernel_t* kernel,
-                     avg_params_t* params);
+                     struct avg_params_t* params);
 
 #endif // __KERNEL_AVG_H

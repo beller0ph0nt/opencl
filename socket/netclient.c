32fc5042253878969fe7880a7cce8d383f4d80ad
beller0ph0nt <kras_den@rambler.ru>
2016-07-31 13:13:27 +0700

    добавлен тестовый пример по работе с сокетами

--- socket/netclient.c
+++ socket/netclient.c
@@ -1,55 +1,64 @@
+#include <netdb.h>
+#include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <errno.h>
-#include <strings.h>
+#include <string.h>
+
 #include <sys/types.h>
 #include <sys/socket.h>
+
 #include <netinet/in.h>
-#include <netdb.h> 
 
 #define BUF_SIZE 256
 
-int main(int argc, char ** argv)
+int main(int argc, char** argv)
 {
-    int sock, port;
-    struct sockaddr_in serv_addr;
-    struct hostent *server;
-    char buf[BUF_SIZE];
-
-    if (argc < 3) 
+    if (argc < 3)
     {
-       fprintf(stderr,"usage: %s <hostname> <port_number>\n", argv[0]);
-       return EXIT_FAILURE;
+        fprintf(stderr, "usage: %s <hostname> <port_number>\n", argv[0]);
+        return EXIT_FAILURE;
     }
-    port = atoi(argv[2]);
-    sock = socket(AF_INET, SOCK_STREAM, 0);
+
+    int port = atoi(argv[2]);
+
+    int sock = socket(AF_INET, SOCK_STREAM, 0);
     if (sock < 0)
     {
-      printf("socket() failed: %d", errno);
-      return EXIT_FAILURE;
+        printf("socket() failed: %s", strerror(errno));
+        return EXIT_FAILURE;
     }
-    server = gethostbyname(argv[1]);
-    if (server == NULL) 
+
+    struct hostent* server = gethostbyname(argv[1]);
+    if (server == NULL)
     {
-      printf("Host not found\n");
-      return EXIT_FAILURE;
+        printf("Host not found\n");
+        return EXIT_FAILURE;
     }
+
+    struct sockaddr_in serv_addr;
     memset((char *) &serv_addr, 0, sizeof(serv_addr));
     serv_addr.sin_family = AF_INET;
-    strncpy((char *)&serv_addr.sin_addr.s_addr, (char *)server->h_addr, server->h_length);
+    strncpy((char *) &serv_addr.sin_addr.s_addr, (char *)server->h_addr, server->h_length);
     serv_addr.sin_port = htons(port);
-    if (connect(sock, &serv_addr, sizeof(serv_addr)) < 0) 
+
+    if (connect(sock, (struct sockaddr*) &serv_addr, sizeof(serv_addr)) < 0)
     {
-      printf("connect() failed: %d", errno);
-      return EXIT_FAILURE;
+        printf("connect() failed: %s", strerror(errno));
+        return EXIT_FAILURE;
     }
+
     printf(">");
-    memset(buf, 0, BUF_SIZE);
-    fgets(buf, BUF_SIZE-1, stdin);
+
+    char buf[BUF_SIZE] = { 0 };
+    fgets(buf, BUF_SIZE - 1, stdin);
     write(sock, buf, strlen(buf));
+
     memset(buf, 0, BUF_SIZE);
     read(sock, buf, BUF_SIZE-1);
+
     printf("%s\n",buf);
+
     close(sock);
+
     return 0;
 }
--- socket/netserver.c
+++ socket/netserver.c
@@ -1,52 +1,63 @@
+#include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <errno.h>
-#include <strings.h>
-#include <sys/types.h> 
+#include <string.h>
+
+#include <sys/types.h>
 #include <sys/socket.h>
+
 #include <netinet/in.h>
 
 #define BUF_SIZE 256
 
-int main(int argc, char ** argv)
+int
+main(int argc, char** argv)
 {
-     int sock, newsock, port, clen;
-     char buf[BUF_SIZE];
      struct sockaddr_in serv_addr, cli_addr;
-     if (argc < 2) 
+
+     if (argc < 2)
      {
          fprintf(stderr,"usage: %s <port_number>\n", argv[0]);
          return EXIT_FAILURE;
      }
-     sock = socket(AF_INET, SOCK_STREAM, 0);
+
+     int sock = socket(AF_INET, SOCK_STREAM, 0);
      if (socket < 0)
      {
-       printf("socket() failed: %d\n", errno);
-       return EXIT_FAILURE;
+         printf("socket() failed: %s\n", strerror(errno));
+         return EXIT_FAILURE;
      }
-     memset((char *) &serv_addr, 0, sizeof(serv_addr));
-     port = atoi(argv[1]);
+
+     memset((char*) &serv_addr, 0, sizeof(serv_addr));
      serv_addr.sin_family = AF_INET;
      serv_addr.sin_addr.s_addr = INADDR_ANY;
+     int port = atoi(argv[1]);
      serv_addr.sin_port = htons(port);
+
      if (bind(sock, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0)
      {
-       printf("bind() failed: %d\n", errno);
-       return EXIT_FAILURE;
+         printf("bind() failed: %s\n", strerror(errno));
+         return EXIT_FAILURE;
      }
+
+     printf("listening...\n");
      listen(sock, 1);
-     clen = sizeof(cli_addr);
-     newsock = accept(sock, (struct sockaddr *) &cli_addr, &clen);
-     if (newsock < 0) 
+
+     printf("accepting request...\n");
+     int clen = sizeof(cli_addr);
+     int newsock = accept(sock, (struct sockaddr *) &cli_addr, &clen);
+     if (newsock < 0)
      {
-       printf("accept() failed: %d\n", errno);
-       return EXIT_FAILURE;
+        printf("accept() failed: %s\n", strerror(errno));
+        return EXIT_FAILURE;
      }
-     memset(buf, 0, BUF_SIZE);
+
+     char buf[BUF_SIZE] = { 0 };
      read(newsock, buf, BUF_SIZE-1);
-     buf[BUF_SIZE] = 0;
      printf("MSG: %s\n", buf);
+
      write(newsock, "OK", 2);
      close(newsock);
+
      close(sock);
 }
